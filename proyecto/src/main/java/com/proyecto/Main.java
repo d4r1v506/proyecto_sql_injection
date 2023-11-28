841cf3f57b9d4832fbcb6401d26813466dafdbee
dario <djfalconi@hotmail.com>
2023-11-27 20:26:12 -0500

    nuevo cambio sql

--- proyecto/src/main/java/com/proyecto/Main.java
+++ proyecto/src/main/java/com/proyecto/Main.java
@@ -1,16 +1,22 @@
 package com.proyecto;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try {
+
+            Scanner scanner = new Scanner(System.in);
             // Establecer la conexión con la base de datos
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase", "username",
-                    "password");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase", "root",
+                    "");
 
             // Crear una sentencia SQL vulnerable a SQL Injection
-            String productId = args[0];
+            // String productId = "1";
+
+            System.out.print("Ingrese el id del producto: ");
+            String productId = scanner.nextLine();
             String sql = "SELECT * FROM productos WHERE id = " + productId;
 
             // Ejecutar la consulta
@@ -21,8 +27,8 @@ public class Main {
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
                 String nombre = resultSet.getString("nombre");
-                double precio = resultSet.getDouble("precio");
-                System.out.println("ID: " + id + ", Nombre: " + nombre + ", Precio: " + precio);
+                // double precio = resultSet.getDouble("precio");
+                System.out.println("ID: " + id + ", Nombre: " + nombre);
             }
 
             // Cerrar la conexión
