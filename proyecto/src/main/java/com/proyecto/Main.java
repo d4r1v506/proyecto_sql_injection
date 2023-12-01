6cb240e1e5ee11ab73539df6ed41ad4e8a27f948
Darius <djfalconi@hotmail.com>
2023-12-01 12:41:45 -0500

    codigo resulto

--- proyecto/src/main/java/com/proyecto/Main.java
+++ proyecto/src/main/java/com/proyecto/Main.java
@@ -7,31 +7,22 @@ public class Main {
     public static void main(String[] args) {
         try {
 
-            Scanner scanner = new Scanner(System.in);
-            // Establecer la conexión con la base de datos
+            Scanner scanner = new Scanner(System.in);            
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabase", "root",
                     "");
-
-            // Crear una sentencia SQL vulnerable a SQL Injection
-            // String productId = "1";
 
             System.out.print("Ingrese el id del producto: ");
             String productId = scanner.nextLine();
             String sql = "SELECT * FROM productos WHERE id = " + productId;
 
-            // Ejecutar la consulta
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
 
-            // Procesar los resultados
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
                 String nombre = resultSet.getString("nombre");
-                // double precio = resultSet.getDouble("precio");
                 System.out.println("ID: " + id + ", Nombre: " + nombre);
             }
-
-            // Cerrar la conexión
             resultSet.close();
             statement.close();
             connection.close();
